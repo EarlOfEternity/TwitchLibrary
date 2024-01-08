fb2664b75a08128c2d258fdacbcc29c267dc018a
EarlOfEternity <dtoliinyk@gmail.com>
2024-01-08 15:41:47 +0200

    Add project files.

---
+++ Twitch/TwitchClient.cs
@@ -0,0 +1,13 @@
+﻿namespace Twitch
+{
+    public class TwitchClient
+    {
+        public string ID { get; }
+        public string Secret { get; }
+        public TwitchClient(string id, string secret)
+        {
+            ID = id;
+            Secret = secret;    
+        }  
+    }
+}
