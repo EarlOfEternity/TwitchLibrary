10fb227de595f6a8d777529f5fddb09ec59beaa7
EarlOfEternity <dtoliinyk@gmail.com>
2024-01-09 11:15:38 +0200

    Added User class

---
+++ Twitch/TwitchClientUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twitch
+{
+    public class TwitchClientUser
+    {
+        public TwitchClient TwitchClient { get; }
+
+        internal TwitchClientUser(TwitchClient twitchClient)
+        {
+            TwitchClient = twitchClient;
+        }
+    }
+}
